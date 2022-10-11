f0d93658fda5703d8b88d7c56bdf008570a3c4a2
JamesJPorter <porterjjames@gmail.com>
2022-10-10 22:53:15 -0700

    working on page population

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,79 +1,81 @@
 let citySearch = [];
 let userInput = [];
+let userInputSaved;
 let coordinates = {
   resultsLat: [],
   resultsLon: [],
 };
 let stringCoords = {
-    lat: [],
-    long: [],
+  lat: [],
+  long: [],
 };
 let formattedCoords = {
-    lat: [],
-    long: [],
+  lat: [],
+  long: [],
 };
 let searchResult;
+let searchResultForUse;
 
-let tempK; 
-let tempF;
+// let tempK;
+// let tempF;
+
+function inIt() {
+  userInputSaved = [];
+  userInputSaved.push(JSON.parse(localStorage.getItem("Searches")));
+  console.log(userInputSaved);
+}
+
+inIt();
 
 $("#citySearchContainer").on("click", "#weatherSubmitBtn", function (event) {
-    event.preventDefault();
-    userInput = $("#citySearchI").val();
-    console.log($("#citySearchI"));
-    console.log(userInput);
-    if (userInput.indexOf(",") !== -1) {
-        userInput = userInput.split(",");
-        // userInputFormatted = userInputFormatted.push(userInput);
-        localStorage.setItem("Searches", JSON.stringify(userInputFormatted));
-    } else if (userInput === "") {
-        localStorage.setItem("Searches", JSON.stringify(userInput));
-    } else {
-        //userInputFormatted = userInputFormatted.push(userInput);
-        localStorage.setItem("Searches", JSON.stringify(userInput));
-    }
-    console.log(userInput);
-    //console.log(userInputFormatted)
-    fetchDataByCity(userInput);
+  event.preventDefault();
+  console.log(userInputSaved);
+  userInput = $("#citySearchI").val();
+  console.log($("#citySearchI"));
+  console.log(userInput);
+  if (userInput.indexOf(",") !== -1) {
+    userInput = userInput.split(",");
+    // userInputFormatted = userInputFormatted.push(userInput);
+    userInputSaved.push(userInput);
+    localStorage.setItem("Searches", JSON.stringify(userInputSaved));
+  } else if (userInput === "") {
+    userInputSaved.push(userInput);
+    localStorage.setItem("Searches", JSON.stringify(userInputSaved));
+  } else {
+    //userInputFormatted = userInputFormatted.push(userInput);
+    userInputSaved.push(userInput);
+    localStorage.setItem("Searches", JSON.stringify(userInputSaved));
+  }
+  console.log(userInput);
+  console.log(userInputSaved);
+  //console.log(userInputFormatted)
+  fetchDataByCity(userInput);
 });
 
 function fetchDataByCity(city) {
-    fetch(
-        "http://api.openweathermap.org/geo/1.0/direct?q=" +
-        city +
-        "&limit=1&appid=214f0628b63be875d9fcdd939008692a"
-        )
-        .then((response) => response.json())
-        .then((data) => {
-            citySearch = data;
-            console.log(data);
-            console.log(citySearch);
-            console.log(citySearch[0].lon);
-            fetchDataByCoords(citySearch);
-        });
-}
-
-function kelvinToFarenheit() {
-    console.log("temp convert", searchResult)
-    let searchResultForUse = JSON.parse(localStorage.getItem("searchResult", searchResult))
-    console.log(searchResultForUse)
-    console.log("Search Result parsed", searchResultForUse)
-    tempK = parseFloat(searchResultForUse.list[0].main.temp);
-    let tempF = (tempK - 273.15) * 1.8 + 32;
-    tempFDisp = tempF.toString()
-    console.log(tempFDisp)
-    tempFDisp = tempFDisp.slice(0, 5)
-    console.log("tempFDisp", tempFDisp)
+  fetch(
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+      city +
+      "&limit=1&appid=214f0628b63be875d9fcdd939008692a"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      citySearch = data;
+        console.log(data);
+        console.log(citySearch);
+      //   console.log(citySearch[0].lon);
+      fetchDataByCoords(citySearch);
+    });
 }
 
 function fetchDataByCoords(coords) {
     console.log(coords);
     console.log(coords[0].lat);
     console.log(coords[0].lon);
-    for (var i = 0; i < coords.length; i++) {
-        coordinates.resultsLat[i] = coords[i].lat.toString();
-        console.log(coordinates);
-        stringCoords.lat.push(coordinates.resultsLat[i]);
+  for (var i = 0; i < coords.length; i++) {
+    coordinates.resultsLat[i] = coords[i].lat.toString();
+    console.log(coordinates);
+    stringCoords.lat.push(coordinates.resultsLat[i]);
     console.log("stringCoords", stringCoords);
     formattedCoords.lat[i] = Number(stringCoords.lat[i].slice(0, 5));
     console.log("formattedCoords", formattedCoords);
@@ -83,10 +85,10 @@ function fetchDataByCoords(coords) {
     console.log("stringCoords", stringCoords);
     formattedCoords.long[i] = Number(stringCoords.long[i].slice(0, 4));
     console.log("formattedCoords", formattedCoords);
-    //let newCoordinates = coordinates.resultsLat[i].slice(3, 10)
+    let newCoordinates = coordinates.resultsLat[i].slice(3, 10)
   }
   for (var i = 0; i < coords.length; i++) {
-    console.log(formattedCoords.lat[i]);
+    // console.log(formattedCoords.lat[i]);
     // fetch('http://api.openweathermap.org/geo/1.0/reverse?lat=' + formattedCoords.lat[i] + '&lon=' + formattedCoords.long[i] + '&limit=1&appid=2d3b3bc21074396280d510a4956a8fe6')
     fetch(
       "http://api.openweathermap.org/data/2.5/forecast?lat=" +
@@ -97,19 +99,50 @@ function fetchDataByCoords(coords) {
     )
       .then((response) => response.json())
       .then((data) => {
-        searchResult = (data);
-        console.log("data", data);
+        searchResult = data;
+        // console.log("data", data);
         console.log("searchResult", searchResult);
-        kelvinToFarenheit()
         localStorage.setItem("searchResult", JSON.stringify(searchResult));
-        console.log("stringified", searchResult)
+        searchResultForUse = JSON.parse(
+          localStorage.getItem("searchResult", searchResult)
+        );
+        kelvinToFarenheit(searchResultForUse);
+        mPStoMPH(searchResultForUse);
+        console.log("searchResultForUse", searchResultForUse);
       });
   }
 }
 
+function kelvinToFarenheit(results) {
+  //searchResultForUse = JSON.parse(localStorage.getItem("searchResult", searchResult))
+  for (var i = 0; i < results.list.length; i++) {
+    //console.log("results", results);
 
+    let tempK = parseFloat(results.list[i].main.temp);
+    let tempF = (tempK - 273.15) * 1.8 + 32;
+    tempFDisp = tempF.toString();
+    // console.log(tempFDisp);
+    tempFDisp = tempFDisp.slice(0, 5);
+    //console.log("tempFDisp", tempFDisp);
+    results.list[i].main.tempFDisp = tempFDisp;
+    //console.log(results.list[i].main.tempFDisp);
+  }
+  localStorage.setItem("searchResult", JSON.stringify(results));
+  console.log("searchResultForUse", results);
+}
 
-function mPStoMPH(windSpeed){
-   let windMph = windSpeed * 2.237
-   console.log("windMph", windMph)
+function mPStoMPH(results) {
+  //searchResultForUse = JSON.parse(localStorage.getItem("searchResult", searchResult))
+  for (var i = 0; i < results.list.length; i++) {
+    let windMph = results.list[i].wind.speed * 2.237;
+    windMphFDisp = windMph.toString();
+    windMphFDisp = windMphFDisp.slice(0, 4);
+    results.list[i].wind.windMphFDisp = windMphFDisp + " MPH";
+  }
+  localStorage.setItem("searchResult", JSON.stringify(results));
+  console.log("windResults", results);
+}
+
+function showForecast(userInputSaved){
+
 }

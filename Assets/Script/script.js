a989d35f9275213feb1b158f9468423673ea3bb7
JamesJPorter <porterjjames@gmail.com>
2022-10-11 02:06:13 -0700

    functioning fetches and dynamic creation of a single element with broken infinite for loop

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -55,8 +55,8 @@ $("#citySearchContainer").on("click", "#weatherSubmitBtn", function (event) {
 function fetchDataByCity(city) {
   fetch(
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    city +
-    "&limit=1&appid=214f0628b63be875d9fcdd939008692a"
+      city +
+      "&limit=1&appid=214f0628b63be875d9fcdd939008692a"
   )
     .then((response) => response.json())
     .then((data) => {
@@ -85,17 +85,17 @@ function fetchDataByCoords(coords) {
     console.log("stringCoords", stringCoords);
     formattedCoords.long[i] = Number(stringCoords.long[i].slice(0, 4));
     console.log("formattedCoords", formattedCoords);
-    let newCoordinates = coordinates.resultsLat[i].slice(3, 10)
+    let newCoordinates = coordinates.resultsLat[i].slice(3, 10);
   }
   for (var i = 0; i < coords.length; i++) {
     // console.log(formattedCoords.lat[i]);
     // fetch('http://api.openweathermap.org/geo/1.0/reverse?lat=' + formattedCoords.lat[i] + '&lon=' + formattedCoords.long[i] + '&limit=1&appid=2d3b3bc21074396280d510a4956a8fe6')
     fetch(
       "http://api.openweathermap.org/data/2.5/forecast?lat=" +
-      stringCoords.lat[i] +
-      "&lon=" +
-      stringCoords.long[i] +
-      "&appid=214f0628b63be875d9fcdd939008692a"
+        stringCoords.lat[i] +
+        "&lon=" +
+        stringCoords.long[i] +
+        "&appid=214f0628b63be875d9fcdd939008692a"
     )
       .then((response) => response.json())
       .then((data) => {
@@ -141,42 +141,94 @@ function mPStoMPH(results) {
   }
   localStorage.setItem("searchResult", JSON.stringify(results));
   console.log("windResults", results);
-  showForecast()
+  showForecast();
 }
 
 function showForecast() {
-  let processedResults = JSON.parse(localStorage.getItem('searchResult'))
-  console.log(processedResults)
-  let forecastCard = $("<div>")
-  forecastCard.attr("class", "col-2 border forecast-card mx-2")
-  $("#5dayContainer").append(forecastCard)
+  let processedResults = JSON.parse(localStorage.getItem("searchResult"));
+  console.log(processedResults);
+  //Banner for search results
+  let bannerDate = processedResults.list[3].dt_txt;
+  bannerDate = bannerDate.split(" ");
+  bannerDate = moment(bannerDate[0]).format("MM/DD/YYYY");
+  let bannerIconImg = $("<img>");
+  bannerIconImg.attr({
+    id: `${processedResults.list[3].weather.id}`,
+    src:
+      "http://openweathermap.org/img/wn/" +
+      `${processedResults.list[3].weather[0].icon}` +
+      ".png",
+    alt: "Weather icon",
+  });
+  $("#city-name-date").text(
+    processedResults.city.name + " (" + bannerDate + ")"
+  );
+  $("#temp").text(processedResults.list[3].main.tempFDisp);
+  $("#wind").text(processedResults.list[3].wind.windMphFDisp);
+  $("#Humidity").text(processedResults.list[3].main.humidity + "%");
 
-  let dateContainer = $("<div>")
-  dateContainer.attr("class", "date-container h3")
-  let dateSpan = $("<span>")
-  dateSpan.text(processedResults.list[3].dt)
-  dateContainer.append(dateSpan)
-  forecastCard.append(dateContainer)
+  console.log(processedResults.list.length);
+  for (var i = 0; i < processedResults.list.length; i + 7) {
+    let forecastCard = $("<div>");
+    forecastCard.attr("class", "col-2 border forecast-card mx-2");
+    $("#5dayContainer").append(forecastCard);
 
-  let tempContainer = $("<div>")
-  tempContainer.attr("class", "temp-container")
-  let tempSpan = $("<span>")
-  tempSpan.text(processedResults.list[3].main.tempFDisp)
-  tempContainer.append(tempSpan)
-  forecastCard.append(tempContainer)
+    console.log("after forecast card");
 
-  let windContainer = $("<div>")
-  windContainer.attr("class", "wind")
-  let windSpan = $("<span>")
-  windSpan.text(processedResults.list[3].wind.windMphFDisp)
-  windContainer.append(windSpan)
-  forecastCard.append(windContainer)
+    let dateContainer = $("<div>");
+    dateContainer.attr("class", "date-container h3");
+    let dateSpan = $("<span>");
+    let date = processedResults.list[i].dt_txt;
+    date = date.split(" ");
+    date = moment(date[0]).format("MM/DD/YYYY");
+    dateSpan.text(date);
+    dateContainer.append(dateSpan);
+    forecastCard.append(dateContainer);
 
-  let humidityContainer = $("<div>")
-  humidityContainer.attr('class', 'humidity')
-  let humiditySpan = $("<span>")
-  humiditySpan.text(processedResults.list[3].main.humidity)
-  humidityContainer.append(humiditySpan)
-  forecastCard.append(humidityContainer)
+    console.log("after date");
+
+    let iconContainer = $("<div>");
+    iconContainer.attr({ class: "icon" });
+    let iconImg = $("<img>");
+    iconImg.attr({
+      id: `${processedResults.list[i].weather.id}`,
+      src:
+        "http://openweathermap.org/img/wn/" +
+        `${processedResults.list[i].weather[0].icon}` +
+        ".png",
+      alt: "Weather icon",
+    });
+    iconContainer.append(iconImg);
+    forecastCard.append(iconContainer);
+
+    console.log("after icon");
+
+    let tempContainer = $("<div>");
+    tempContainer.attr("class", "temp-container");
+    let tempSpan = $("<span>");
+    tempSpan.text(processedResults.list[i].main.tempFDisp);
+    tempContainer.append(tempSpan);
+    forecastCard.append(tempContainer);
+
+    console.log("after temp");
+
+    let windContainer = $("<div>");
+    windContainer.attr("class", "wind");
+    let windSpan = $("<span>");
+    windSpan.text(processedResults.list[i].wind.windMphFDisp);
+    windContainer.append(windSpan);
+    forecastCard.append(windContainer);
+
+    console.log("after wind");
+
+    let humidityContainer = $("<div>");
+    humidityContainer.attr("class", "humidity");
+    let humiditySpan = $("<span>");
+    humiditySpan.text(processedResults.list[i].main.humidity + "%");
+    humidityContainer.append(humiditySpan);
+    forecastCard.append(humidityContainer);
+    console.log("end of loop");
+  }
+  return;
+  console.log("exited loop");
 }
-
